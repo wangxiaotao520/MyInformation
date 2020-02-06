57720c1fb80b8405cb8f8df4571333ef11cf68db
wangxiaotao <18635423763@163.com>
2020-02-06 20:11:28 +0800

    测试家里的网络提交

---
+++ app/src/main/java/com/wangxiaotao/myinformation/SplashActivity.java
@@ -0,0 +1,11 @@
+package com.wangxiaotao.myinformation;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Description: 启动页
+ * created by wangxiaotao
+ * 2019/12/3 0003 上午 8:53
+ */
+public class SplashActivity extends AppCompatActivity{
+}
